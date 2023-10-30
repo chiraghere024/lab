33fea154fabb316fccdff14ae26c38eadfcb2834
chiraghere024 <131499752+chiraghere024@users.noreply.github.com>
2023-10-30 06:43:47 +0530

    Add files via upload

---
+++ 6b.js
@@ -0,0 +1,27 @@
+    $(document).ready(function () {
+    $("#country").change(function () {
+    const selectedCountry = $(this).val();
+    if (selectedCountry !== "") {
+    $.ajax({
+    url: `get-cities.php?country=${selectedCountry}`, type: "GET", dataType: "json", success: function (data) {
+    const cityDropdown = $("#city");
+    cityDropdown.empty();
+    if (data.length > 0) {
+    cityDropdown.append("<option value=''>Select</option>");
+    $.each(data, function (index, city) {
+    cityDropdown.append(
+    $("<option></option>").attr("value", city).text(city)
+    );
+    });
+    } else {
+    cityDropdown.append("<option value=''>No cities available</option>");
+    }
+    }, error: function () {
+    console.error("Error fetching cities.");
+    }, });
+    } else {
+    // Reset city dropdown if no country is selected
+    $("#city").html("<option value=''>Select a country first</option>");
+    }
+    });
+    });
